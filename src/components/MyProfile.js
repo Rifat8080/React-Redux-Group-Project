c88de6810d1bded90768a29839498cfbaa3feb7b
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-07-26 14:41:53 +0600

    Fixed my profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -6,33 +6,47 @@ const MyProfile = () => {
   const missionsStore = useSelector((state) => state.missions);
   const { missions } = missionsStore;
   const myMissions = missions.filter((mission) => mission.joined === true);
+  const rockets = useSelector((state) =>
+    state.rockets.rockets.filter((rocket) => rocket.reserved === true)
+  );
 
   return (
-    <div className="container">
-      <div className="row justify-content-evenly">
-        <div className="col-12">
-          <h1 className="p-1">My Missions</h1>
-          <div>{!myMissions.length ? 'No Missions Joined' : ' '}</div>
-          <div>
-            {myMissions.map((mission) => (
-              <div
-                className="border p-3 d-flex justify-content-between align-items-center"
-                key={mission.id}
-              >
-                <div>{mission.name}</div>
-                <button
-                  type="button"
-                  className="btn btn-outline-danger"
-                  onClick={() => dispatch(joinMission(mission))}
+    <>
+      <div className="container">
+        <div className="row justify-content-evenly">
+          <div className="col-12">
+            <h1 className="p-1">My Missions</h1>
+            <div>{!myMissions.length ? 'No Missions Joined' : ' '}</div>
+            <div>
+              {myMissions.map((mission) => (
+                <div
+                  className="border p-3 d-flex justify-content-between align-items-center"
+                  key={mission.id}
                 >
-                  Leave Mission
-                </button>
-              </div>
-            ))}
+                  <div>{mission.name}</div>
+                  <button
+                    type="button"
+                    className="btn btn-outline-danger"
+                    onClick={() => dispatch(joinMission(mission))}
+                  >
+                    Leave Mission
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <div className="rockets-profile">
+        <h2>My Rockets</h2>
+        <ul className="table">
+          {rockets.map((rocket) => (
+            <li key={rocket.id}>{rocket.name}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
