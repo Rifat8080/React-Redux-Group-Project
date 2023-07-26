ba592788e0d6ff182fca6e488d0b241b35863883
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-07-26 12:58:00 +0600

    My profile filter and fetch data functionality

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,2 +1,39 @@
-const MyProfile = () => <div>Empty page content</div>;
+import { useSelector, useDispatch } from 'react-redux';
+import { joinMission } from '../redux/missions/missionSlice';
+
+const MyProfile = () => {
+  const dispatch = useDispatch();
+  const missionsStore = useSelector((state) => state.missions);
+  const { missions } = missionsStore;
+  const myMissions = missions.filter((mission) => mission.joined === true);
+
+  return (
+    <div className="container">
+      <div className="row justify-content-evenly">
+        <div className="col-6">
+          <h1 className="p-2">My Missions</h1>
+          <div>{!myMissions.length ? 'No Missions Joined' : ' '}</div>
+          <div>
+            {myMissions.map((mission) => (
+              <div
+                className="border p-3 d-flex justify-content-between"
+                key={mission.id}
+              >
+                <div>{mission.name}</div>
+                <button
+                  type="button"
+                  className="btn btn-outline-danger"
+                  onClick={() => dispatch(joinMission(mission))}
+                >
+                  Leave Mission
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default MyProfile;
