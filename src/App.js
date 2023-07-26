cd7caf08908804798c8c4e2f683bc69f624ea40d
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-07-26 12:14:40 +0600

    Fixing functionalities

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Rockets from './components/Rockets';
-// import Missions from './components/MissionsList';
 import MyProfile from './components/MyProfile';
 import MissionsList from './components/MissionsList';
 
